ca2d26bfa51de82e476d1431116583baf527bae7
Dang Xuan Anh <dxanh97@gmail.com>
2021-04-30 23:29:49 +0700

    chore: add alias paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: 'dxanh97',
   },
-  plugins: ['gatsby-plugin-styled-components', 'gatsby-plugin-typescript'],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          '@dxanh97/components': 'src/components',
+        },
+        extensions: [],
+      },
+    },
+  ],
 };
