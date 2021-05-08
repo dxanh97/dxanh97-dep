f1d89fba8ce133d8bc95cc1657a81da570f5fccc
Dang Xuan Anh <dxanh97@gmail.com>
2021-05-08 17:30:35 +0700

    feat: implement `PorfolioHeader`

--- src/components/PortfolioHeader.tsx
+++ src/components/PortfolioHeader.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-
 import styled from 'styled-components';
+import { down } from 'styled-breakpoints';
 
-const Header = styled.div`
-  padding: 40px;
-  font-size: 50px;
+const Wrapper = styled.div`
+  text-align: center;
+  border-bottom: 1px solid;
+  & > div.name {
+    padding: 20px;
+    padding-bottom: 8px;
+    font-size: 50px;
+    ${down('md')} {
+      padding: 16px;
+      padding-bottom: 4px;
+      font-size: 36px;
+    }
+  }
+  & > div.title {
+    padding: 0 20px 20px;
+    font-size: 20px;
+    ${down('md')} {
+      padding: 0 16px 16px;
+      font-size: 16px;
+    }
+  }
 `;
 
-const PortfolioHeader: React.FC = () => <Header>dxanh97</Header>;
+const PortfolioHeader: React.FC = () => (
+  <Wrapper>
+    <div className="name">Đặng Xuân Anh</div>
+    <div className="title">Software Engineer</div>
+  </Wrapper>
+);
 
 export default PortfolioHeader;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
+import styled from 'styled-components';
 import { Container, PortfolioHeader } from '@dxanh97/components';
 import '../styles.css';
+
+const Wrapper = styled.div`
+  border: 1px solid;
+  border-radius: 8px;
+  background: #fef8ec;
+  /* text-transform: lowercase; */
+`;
 
 const IndexPage = () => {
   return (
     <Container>
+      <Wrapper>
       <PortfolioHeader />
+      </Wrapper>
     </Container>
   );
 };
