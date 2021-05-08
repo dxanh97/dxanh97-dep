3665583436743f0b75d10bd5a3cb8ae5fd0a4305
Dang Xuan Anh <dxanh97@gmail.com>
2021-05-08 16:58:31 +0700

    feat: implement `Container`

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { up, down } from 'styled-breakpoints';
 
 const StyledDiv = styled.div`
-  background: #fef8ec;
+  margin: auto;
+  margin-top: 20px;
+  ${down('md')} {
+    width: calc(100% - 2em);
+    margin-top: 1em;
+  }
+  ${up('md')} {
+    width: 723px;
+  }
+  ${up('lg')} {
+    width: 933px;
+  }
+  ${up('xl')} {
+    width: 1127px;
+  }
 `;
 
 const Container: React.FC = ({ children }) => <StyledDiv>{children}</StyledDiv>;
