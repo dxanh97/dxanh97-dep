f0fed82ce625bd6801c76d28696d9bf1a1c5633b
Dang Xuan Anh <dxanh97@gmail.com>
2021-05-11 12:56:28 +0700

    chore: update WorkExperiences text

--- src/components/WorkExperiences.tsx
+++ src/components/WorkExperiences.tsx
@@ -31,6 +31,10 @@ const Wrapper = styled.div`
     }
     & > p.title {
       margin: 8px 0;
+      & a {
+        text-decoration: underline 1px;
+        color: #000000;
+      }
     }
     & > p.time {
       margin: 8px 0;
@@ -48,9 +52,15 @@ const WorkExperiences: React.FC = () => (
     <SectionHeader title="Work Experiences" />
     <div className="block">
       <p className="title">
-        <b>Full-Stack Developer</b> at <b>Bakco</b>
+        <b>Full-Stack Developer</b>
+        <span> at </span>
+        <b>
+          <a target="_blank" href="https://www.facebook.com/hisoftcompany/">
+            HiSoft
+          </a>
+        </b>
       </p>
-      <p className="time">Sep 2017 — Sep 2020</p>
+      <p className="time">Mar 2017 — Sep 2017</p>
       <p className="description">
         Maintain and develop new features for <b>QTSC OMS</b> (
         <b>Quang Trung Software City Operations Management Suite</b>) mobile app
@@ -58,24 +68,52 @@ const WorkExperiences: React.FC = () => (
       </p>
     </div>
     <div className="block">
-      <b>Full-Stack Developer</b> at <b>Bakco</b>
-      <p className="time">Sep 2017 — Sep 2020</p>
+      <p className="title">
+        <b>Full-Stack Developer</b>
+        <span> at </span>
+        <b>
+          <a target="_blank" href="http://www.bakco.com.vn/">
+            Bakco
+          </a>
+        </b>
+      </p>
+      <p className="time">Nov 2017 - July 2018 & July 2019 — Sep 2020</p>
       <p className="description">
         Responsible for developing <b>Hospital Management</b> module to help
         hospitals easily manage their own internal resources and connect with
         others using one platform.
       </p>
-    </div>
-    <div className="block">
-      <b>Full-Stack Developer</b> at <b>Bakco</b>
-      <p className="time">Sep 2017 — Sep 2020</p>
+      <br />
       <p className="description">
         Lead Front-End team of 2 to develop several apps like:
         <br />• <b>PHR</b> (<b>Personal Health Record</b>) letting user to track
         their medical records and body indexes
-        <br /> • <b>CDS</b> (<b>Control Disease System</b>) for HCDC to record
-        and manage the Covid-19 infection chains in Ho Chi Minh city.
-        Technologies used: React, Redux, TypeScript,
+        <br />• <b>CDS</b> (<b>Control Disease System</b>) for HCDC to record
+        and manage the Covid-19 infection chains in <b>HCM</b> city.
+      </p>
+    </div>
+    <div className="block">
+      <p className="title">
+        <b>Full-Stack Developer</b>
+        <span> at </span>
+        <b>
+          <a target="_blank" href="https://www.codelink.io/">
+            CodeLink
+          </a>
+        </b>
+      </p>
+      <p className="time">Oct 2020 - Present</p>
+      <p className="description">
+        Update the company's website and add a ci config to build automatically
+        using <b>CircleCI</b>
+      </p>
+      <br />
+      {/* prettier-ignore */}
+      <p className="description">
+        Outsourcing for <b>Electronic Arts</b> to help build an application that
+        manage marketing assets (videos, images, ...)
+        and use <b>machine learning</b> and <b>serverless</b> to
+        create audience targeted marketing videos
       </p>
     </div>
   </Wrapper>
