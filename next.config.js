5a7b7c6c1e237773420d62257f6d85ebb3f04c57
Xanh <dxanh97@gmail.com>
2022-03-17 16:26:53 +0700

    chore: clean-up repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  exportPathMap: async (defaultPathMap) => {
+    return defaultPathMap;
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
