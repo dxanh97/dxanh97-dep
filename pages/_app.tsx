8507fdba997e32e6dde8bca26567c305088754cd
Xanh <dxanh97@gmail.com>
2022-03-31 16:28:43 +0700

    chore: add running script for GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-17C9F9KBGF" />
+      <Script
+        id=""
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-17C9F9KBGF', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </Layout>
   );
