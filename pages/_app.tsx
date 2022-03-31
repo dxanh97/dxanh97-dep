ec6d43455de9ce081277f7e0586176a8ae677de8
Xanh <dxanh97@gmail.com>
2022-03-31 15:56:18 +0700

    chore: add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import Layout from '../components/common/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-17C9F9KBGF"
+        strategy="afterInteractive"
+      />
       <Component {...pageProps} />
     </Layout>
   );
