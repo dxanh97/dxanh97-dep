45e352bdb7df034b7c40c4124627865e1aa609f6
Xanh <dxanh97@gmail.com>
2022-03-31 16:22:43 +0700

    chore: update GA script import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,7 @@ import Layout from '../components/common/Layout';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-17C9F9KBGF"
-        strategy="afterInteractive"
-      />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-17C9F9KBGF" />
       <Component {...pageProps} />
     </Layout>
   );
